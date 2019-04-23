5475db7b803fdcf562ae8148da99f3d931fd3359
Ivan Zlatev <ivan.zlatev@securax.org>
2019-04-24 00:27:22 +0300

    Fixing some comments
    Fixing some comments

--- TrackerLibrary/TrackerLibrary/SQLConnector.cs
+++ TrackerLibrary/TrackerLibrary/SQLConnector.cs
@@ -6,9 +6,17 @@ namespace TrackerLibrary
 {
     public class SQLConnector : IDataConnection
     {
+        /// <summary>
+        /// Saves a new prize to the database
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>The prize information, including the unique identifier.</returns>
+
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            throw new NotImplementedException();
+            model.id = 1;
+
+            return model;
         }
     }
 }
